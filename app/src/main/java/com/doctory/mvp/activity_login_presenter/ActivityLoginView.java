7047b39f5338187e66935753170755555924e7a3
Emad <emadmagdy.developer@gmail.com>
2020-11-02 17:06:23 +0200

    Initial commit

---
+++ app/src/main/java/com/doctory/mvp/activity_login_presenter/ActivityLoginView.java
@@ -0,0 +1,5 @@
+package com.doctory.mvp.activity_login_presenter;
+
+public interface ActivityLoginView {
+    void onLoginValid();
+}
---
+++ app/src/main/java/com/doctory/mvp/activity_splash_mvp/SplashView.java
@@ -0,0 +1,9 @@
+package com.doctory.mvp.activity_splash_mvp;
+
+import android.view.View;
+
+public interface SplashView {
+    void onNavigateToLanguageActivity();
+    void onNavigateToLoginActivity();
+    void onNavigateToHomeActivity();
+}
